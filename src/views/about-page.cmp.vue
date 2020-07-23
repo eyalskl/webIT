59093da280227b7b3016eee82e3d2ebdea470bc6
Eyal Barkai <eyalskl18@gmail.com>
2020-07-23 03:14:54 +0300

    asd

--- src/views/about-page.cmp.vue
+++ src/views/about-page.cmp.vue
@@ -1,21 +1,17 @@
 <template>
 <div class="about-page app-main">
     <nav-bar />
-    <h3> This is an about page </h3>
+    <h3> About </h3>
+    <h3> Us </h3>
   </div>
 </template>
 
 <script>
 import navBar from '@/components/nav-bar.cmp.vue';
-
-
 export default {
-  
+
   components: {
     navBar
   }
 }
 </script>
-
-<style>
-</style>
