da8d43be44b8c5c87ac4ae57f50e9ebf49131b54
Nadav_Frank <frankos333@gmail.com>
2020-07-25 18:09:56 +0200

    forms forms forms

--- src/views/site-editor.cmp.vue
+++ src/views/site-editor.cmp.vue
@@ -1,5 +1,7 @@
 <template>
-  <container class="editor-container flex column animate__animated animate__fadeIn">
+  <container
+    class="editor-container flex column animate__animated animate__fadeIn"
+  >
     <nav-bar />
     <div class="editor flex">
       <element-dashboard
@@ -17,91 +19,100 @@
 </template>
 
 <script>
-import navBar from '@/components/nav-bar.cmp.vue';
-import siteWorkspace from '../components/site-workspace.cmp.vue';
-import elementDashboard from '@/components/element-dashboard.cmp.vue';
-import { templateService } from '@/services/template-service.js';
-import { utilService } from '@/services/util.service.js';
-import { Container, Draggable } from 'vue-smooth-dnd';
-import { applyDrag, generateItems } from '@/assets/drag-test.js';
-import { eventBus, ADD_SAMPLE, EDIT_ELEMENT, CLONE_ELEMENT, REMOVE_ELEMENT, MOVE_ELEMENT } from '@/services/event-bus.service.js';
-const _ = require('lodash');
+import navBar from "@/components/nav-bar.cmp.vue";
+import siteWorkspace from "../components/site-workspace.cmp.vue";
+import elementDashboard from "@/components/element-dashboard.cmp.vue";
+import { templateService } from "@/services/template-service.js";
+import { Container, Draggable } from "vue-smooth-dnd";
+import { applyDrag, generateItems } from "@/assets/drag-test.js";
+
+import {
+  eventBus,
+  ADD_SAMPLE,
+  EDIT_ELEMENT,
+  CLONE_ELEMENT,
+  REMOVE_ELEMENT,
+  MOVE_ELEMENT,
+} from "@/services/event-bus.service.js";
+
+const _ = require("lodash");
 
 export default {
-  name: 'site-editor',
+  name: "site-editor",
+
   data() {
     return {
       samples: {},
-      siteToEdit: null
-    }
+
+      }
+      
+    
   },
   computed: {
-    // siteToEdit(){
-    //     return this.$store.getters.site;
-    // }
+    siteToEdit(){
+        return this.$store.getters.site;
+    }
   },
-  created() {
-    this.$store.commit({ type: 'setEditMode', editMode: true });
+  async created() {
+    this.$store.commit({ type: "setEditMode", editMode: true });
     this.loadSite();
-    this.samples = templateService.getSamplesOf('section');
+    this.samples = templateService.getSamplesOf("section");
     eventBus.$on(ADD_SAMPLE, (sample) => this.addSample(sample));
     eventBus.$on(CLONE_ELEMENT, (element) => this.clone(element));
-    eventBus.$on(REMOVE_ELEMENT, (elementId) => {
-      this.remove(elementId)
-      });
-    eventBus.$on(MOVE_ELEMENT, (elementId, direction) => {
+    eventBus.$on(REMOVE_ELEMENT, (elementId) => this.remove(elementId));
+    eventBus.$on(MOVE_ELEMENT, (elementId, direction) =>
       this.moveElement(elementId, direction)
-    });
+    );
   },
   methods: {
     async loadSite() {
-      const templateId = this.$route.params.id;
-      const site = await this.$store.dispatch({ type: 'loadSite', id: templateId,});
-      this.siteToEdit = site
+      const site = await this.$store.dispatch({
+        type: "loadSite",
+        id: this.$route.params.id,
+      });
     },
     getSamplesToShow(listName) {
       this.samples = templateService.getSamplesOf(listName);
     },
     addSample(sample) {
       this.siteToEdit.cmps.unshift(sample);
-      this.$store.commit({ type: 'setSite', site: this.siteToEdit });
+      this.$store.commit({ type: "setSite", site: this.siteToEdit });
     },
     getElementSamples(element) {
       this.samples = templateService.getSamplesOf(element);
     },
     moveElement(elementId, direction) {
-      let cmps = this.siteToEdit.cmps;
-      let idx = cmps.findIndex(cmp => cmp.id === elementId);
-      if (idx === -1) return
-      if (direction === 'down' && idx + 1 < cmps.length) {
-        let cmp = cmps[idx];
+      const cmps = this.siteToEdit.cmps;
+      const idx = cmps.findIndex((cmp) => cmp.id === elementId);
+      if (direction === "down" && idx + 1 < cmps.length) {
+        const cmp = cmps[idx];
         cmps.splice(idx, 1, cmps[idx + 1]);
         cmps.splice(idx + 1, 1, cmp);
-      } else if (direction === 'up' && idx !== 0) {
-        let cmp = cmps[idx];
+      } else if (direction === "up" && idx !== 0) {
+        const cmp = cmps[idx];
         cmps.splice(idx, 1, cmps[idx - 1]);
         cmps.splice(idx - 1, 1, cmp);
       }
-      this.$store.commit({ type: 'setSite', site: this.siteToEdit });
+      this.$store.commit({ type: "setSite", site: this.siteToEdit });
     },
     clone(element) {
-      let idx = this.siteToEdit.cmps.findIndex((cmp) => cmp.id === element.id);
-      if (idx === -1) return
+      const cmps = this.siteToEdit.cmps;
+      const idx = cmps.findIndex((cmp) => cmp.id === element.id);
       let clone = _.cloneDeep(element);
-      clone.id = utilService.makeId();
+      clone.id = templateService.makeId();
       clone = templateService.addIds(clone);
-      this.siteToEdit.cmps.splice(idx, 0, clone);
-      this.$store.commit({ type: 'setSite', site: this.siteToEdit });
+      cmps.splice(idx, 0, clone);
+      this.$store.commit({ type: "setSite", site: this.siteToEdit });
     },
     remove(elementId) {
-      let idx = this.siteToEdit.cmps.findIndex(cmp => cmp.id === elementId);
-      if (idx === -1) return
-      this.siteToEdit.cmps.splice(idx, 1);
-      this.$store.commit({ type: 'setSite', site: this.siteToEdit });
+      const cmps = this.siteToEdit.cmps;
+      const idx = cmps.findIndex((cmp) => cmp.id === elementId);
+      cmps.splice(idx, 1);
+      this.$store.commit({ type: "setSite", site: this.siteToEdit });
     },
   },
   watch: {
-    '$route.params.id'() {
+    "$route.params.id"() {
       this.loadSite();
     },
   },
@@ -112,8 +123,5 @@ export default {
     Draggable,
     navBar,
   },
-  destroyed() {
-      this.$store.commit({ type: 'setSite', site: null });
-  },
-}
+};
 </script>
